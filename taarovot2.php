c99ed16b0070e83179b49537e1675c075dd80f93
Vadim <zubenko@fruitfulcode.com>
2012-12-21 12:02:19 +0200

    add files

---
+++ taarovot2.php
@@ -0,0 +1,72 @@
+<?php
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<title>CUPPING</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	
+	<link rel="stylesheet" type="text/css" href="_media/css/layout.css" />
+	
+	<script type="text/javascript" src="_media/js/jquery-1.8.3.min.js"></script>
+	<script type="text/javascript" src="_media/js/main.js"></script>
+	<style type="text/css">
+		
+	</style>
+
+</head>
+<body>
+	<?php include_once ("_inc/top.inc.php"); ?>
+		<div id="wrapper">
+			<div class="content3">
+				<div id="india">
+					<div id="char">
+					<h2>הודו</h2>
+						<ul class="ulchar">
+							<li id="char1"><div id="leftchar">הקפה מהודו מאופיין בטעמי אדמה, חומציות</br>נמוכה ונחשב כקפה עדין</div><div id="rightchar"><img src="_media/images/coffee/taarovot2/pic1.png"</div></li>
+							<li id="char2"><div id="leftchar">מתאים להכנת אספרסו, טורקי ופילטר</div><div id="rightchar"><img src="_media/images/coffee/taarovot2/pic2.png"</div></li>
+							<li id="char3"><div id="leftchar"><p>תערובת זו מחברת את הזנים האיכותיים פרצ‘מנט, פלנטיישן וקפי</br>
+							רויאל, ומשמרת את טעמיהם האופייניים. נקלית בקלייה בינונית</br>
+							חזקה (קלייה וינאית) ואף מדגישה אותם.</br>
+							הודו היא יצרנית הקפה השנייה באסיה והשמינית בעולם.
+							</br>
+							</br>
+							יבול הקפה הראשון בהודו היה בשנת 1610, הודות לקדוש בשם
+							באבא בודן, שבדרכו למכה עבר בתימן והצליח להבריח 7 זרעים של
+							קפה שהיו עד אז נחלתה הבלעדית של יבשת אפריקה. כשחזר 
+							להודו, זרע אותם בגבעות שזוכות עד היום להיקרא על שמו.</p>
+							</div>
+							<div id="rightchar"><img src="_media/images/coffee/taarovot2/pic3.png"></div></li>
+						</ul>
+							<div class="arrows">
+							<a id="arrows1" href="#"><img src="_media/images/coffee/taarovot2/1.png"></a>
+							<a id="arrows2" href="#"><img src="_media/images/coffee/taarovot2/22.png"></a>
+							<a id="arrowleft"  href="#"><img src="_media/images/coffee/taarovot2/arrowleft.png"></a>
+							</div>			
+					</div>
+				</div>
+				
+				<div id="signs">
+					<div id="products2"><a href="#"><img src="_media/images/coffee/taarovot/name6.png" alt="photo"></a></div>
+					<div id="products2"><a href="#"><img src="_media/images/coffee/taarovot/name1.png" alt="photo"></a></div>
+					<div id="products2"><a href="#"><img src="_media/images/coffee/taarovot/name2.png" alt="photo"></a></div>
+					<div id="products2"><a href="#"><img src="_media/images/coffee/taarovot/name7.png" alt="photo"></a></div>
+					<div id="products2"><a href="#"><img src="_media/images/coffee/taarovot/name4.png" alt="photo"></a></div>
+					<div id="products2"><a href="#"><img src="_media/images/coffee/taarovot/name5.png" alt="photo"></a></div>
+				</div>
+			</div>
+				<div class="sidebar"> 
+					<div id="mix">
+						<h2>התערובות שלנו</h2>
+					</div>
+						<img src="_media/images/coffee/taarovot2/coffee.png" alt="">
+				</div>	
+				<div id="bottom">
+						<a href="#" id="arrow1">חזרה לכל התערובות<img src="_media/images/coffee/cupping/arrow1.png" alt=""></a>
+				</div>
+		</div>
+	
+	
+	<?php include_once ("_inc/footer.inc.php"); ?>
+</body>
+</html>
